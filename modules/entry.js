5f42daa3d796286e05cbe94d48e8b05a334f471a
abcdan <daniel@lngzl.nl>
2022-09-08 10:08:33 +0200

    test

---
+++ modules/entry.js
@@ -0,0 +1,7 @@
+module.exports = function (iteration, addTime, selectTime, editTime, deleteTime) {
+    this.iteration = iteration
+    this.addTime = addTime
+    this.selectTime =  selectTime
+    this.editTime = editTime
+    this.deleteTime = deleteTime
+}
